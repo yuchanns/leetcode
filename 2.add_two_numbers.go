51f719656048921edccd65cbfaa02206362f1a0e
Hanchin Hsieh <me@yuchanns.xyz>
2022-11-06 05:15:27 +0800

    update
    Signed-off-by: Hanchin Hsieh <me@yuchanns.xyz>

--- 2.add_two_numbers.go
+++ 2.add_two_numbers.go
@@ -26,15 +26,14 @@ func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
 			current = current.Next
 		}
 		if l1 != nil {
-			current.Val = (current.Val + l1.Val) % 10
 			carry += l1.Val
 			l1 = l1.Next
 		}
 		if l2 != nil {
-			current.Val = (current.Val + l2.Val) % 10
 			carry += l2.Val
 			l2 = l2.Next
 		}
+		current.Val = carry % 10
 		carry /= 10
 		if result == nil {
 			result = current
